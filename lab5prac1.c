0717df8ad1ca6c72e1a68543d7e4db7e3c67a6ed
devwitharyan <149342937+devwitharyan@users.noreply.github.com>
2024-08-04 16:16:51 +0530

    Update lab5prac1.c

--- lab5prac1.c
+++ lab5prac1.c
@@ -1,128 +1,129 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int push(int [],int,int,int);
+int push(int [], int, int, int);
 int pop(int [],int,int);
 void peep(int [],int,int,int);
 void change(int[], int, int, int);
-void display(int [],int, int);
+void display(int [], int, int);
 
 void main(){
-	int choice, a[50], top = -1, n=5, x, i;
+    int a[100], n, x, choice, top = -1, i;
 
-	do{
-		printf("1 for push.\n");
-		printf("2 for pop.\n");
-		printf("3 for peep.\n");
-		printf("4 for change.\n");
-		printf("5 for display.\n");
-		printf("0 for exit.\n");
+    printf("Enter size of stack: ");
+    scanf("%d", &n);
 
-		scanf("%d", &choice);
+    
+    do{
+        printf("1 for push\n");
+        printf("2 for pop\n");
+        printf("3 for peep\n");
+        printf("4 for change\n");
+        printf("5 for display\n");
+        printf("0 for exit\n");
 
-		switch(choice){
-			case 1:
-				printf("push operation \n");
-				printf("Enter value\n");
-		 		scanf("%d",&x);
-		 		top=push(a,top,x,n);
-		 		break;
-			case 2:
-				printf("POP operation\n");
-				top = pop(a, top, n);
-				break;
-			case 3:
-				printf("PEEP operation\n");
-				printf("Enter number from top: \n");
-				scanf("%d", &i);
-				peep(a, top, i, n);
-				break;
-			case 4:
-				printf("CHANGE operation\n");
-				printf("Enter position & value:\n");
-				scanf("%d %d", &i, &x);
-				change(a, top, i, x);
-				break;
-			case 5:
-				printf("Display operation\n");
-				display(a, top, n);
-				break;
-			case 0:
-				exit(0);
-				break;
-			default:
-		 		printf("Enter valid choice");						
-		 	}
+        scanf("%d", &choice);
 
-	}while(1);
-
+        switch (choice)
+        {
+        case 1:
+        	printf("PUSH operation!\n");
+            printf("Enter element to push: ");
+            scanf("%d", &x);
+            top=push(a, top, x, n);
+            break;
+        case 2:
+            printf("POP operation!\n");
+            top = pop(a, top, n);
+            break;
+        case 3:
+            printf("PEEP operation!\n");
+            printf("Enter value from top: \n");
+            scanf("%d", &i);
+            peep(a, top, i, n);
+            break;
+        case 4:
+        	printf("Change operation!\n");
+        	printf("Enter value and position to change: \n");
+        	scanf("%d %d", &x, &i);
+        	change(a, top, i, x);
+        	break;
+        case 5:
+            printf("Display operation. \n");
+            display(a, top, n);
+            break;
+        case 0:
+			exit(0);
+			break;
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+    }while(1);    
 }
 
-
-int push(int a[],int top,int x,int n){
-	printf("TOP = %d\n", top);
-	if(top >= n-1)
-		printf("stack is overflow\n");
-	else{
-		top = top + 1;
-		a[top] = x;
-	}
-	return top;
+int push(int a[], int top, int x, int n){
+    printf("top is: %d\n", top);
+    if(top >= n-1){
+        printf("Stack Overflow\n");
+    }
+    else{
+        top = top + 1;
+        a[top] = x;
+    }
+    return top;
 }
 
 int pop(int a[],int top,int n){
-	printf("TOP = %d\n", top);
 	int value;
-	if(top < 0){
-		printf("Underflow\n");
+	printf("top is: %d\n", top);
+	if(top == -1){
+		printf("Stack is underflow or empty \n");
 	}
 	else{
 		value = a[top];
 		top--;
-		printf("POPPED: %d\n", value);
-		return top;
+		printf("POPPED: %d \n", value);
 	}
+	return top;
 }
 
 void peep(int a[],int top,int i,int n){
-	printf("TOP = %d\n", top);
-	int temp;
+	printf("Top is: %d\n", top);
+	int value;
 	if(top < 0){
-		printf("Stack is Underflow");
+		printf("Stack is empty.\n");
 	}
 	else{
-		if(top-i+1 < 0){
-			printf("Stack is underflow 2\n");
+		if(top-i + i<0){
+			printf("Stack is underflow.\n");
 		}
 		else{
-			temp = a[top-i+1];
-			printf("ith element is: %d\n", temp);
+			value = a[top-i+1];
+			printf("Peeped value is: %d\n", value);
 		}
 	}
 }
 
 void change(int a[], int top, int i, int x){
-	printf("TOP = %d\n", top);
+	printf("Top is: %d\n", top);
 	if(top < 0){
-		printf("Stack is empty");
-	}
+		printf("Stack is empty.\n");
+	}	
 	else{
-		if(top-i+1 < 0){
-			printf("Stack is Underflow\n");
+		if(top-i + 1 < 0){
+			printf("Stack is underflow.\n");
 		}
 		else{
-
 			a[top-i+1] = x;
-			printf("Change is done.\n");
 		}
 	}
 }
 
-
 void display(int a[],int top, int n){
 	int i;
 	if(top <= 0){
-		printf("Stack is empty");
+		printf("Stack is empty\n");
 	}
 	else{
 		for(i=top;i>=0;i--){
